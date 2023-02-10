35384ec07cd60a642061268e11e1634cb2f6c839
Egor <chuvak.postal.84@mail.ru>
2023-02-10 21:37:47 +0300

    Задачка с днями недели

---
+++ Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу которая будет выдавать название дня недели по заданному номеру
+// 3-> Среда
+// 5-> Пятница
+
+Console.WriteLine("Input number (Введите номер)");
+int a = Convert.ToInt32(Console.ReadLine());
+int c = 5;
+switch (a)
+{
+    case 1:
+        Console.WriteLine("Monday (Понедельник)");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday (Вторник)");
+        break;
+    case 3:
+        Console.WriteLine("Wednesday (среда)");
+        break;
+    case 4:
+        Console.WriteLine("Thursday (Четверг)");
+        break;
+    case 5:
+        Console.WriteLine("Friday (Пятница)");
+        break;
+    case 6:
+        Console.WriteLine("Saturday (Суббота)");
+        break;
+    case 7:
+        Console.WriteLine("Sunday (Воскресение)");
+        break;
+    default:
+        Console.WriteLine("There is no such a weekday (Нет такого дня недели)");
+        break;
+}
